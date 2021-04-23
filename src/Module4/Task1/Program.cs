3d2956cbc4e49acea5a0f17f68f95dde9761b961
DmitryPanaskov <44893730+DmitryPanaskov@users.noreply.github.com>
2021-04-23 21:07:22 +0300

    Module4

--- src/Module4/Task1/Program.cs
+++ src/Module4/Task1/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            Console.WriteLine("Enter a string and press ENTER. ");
+            Console.WriteLine("Enter an 'exit' and press ENTER for EXIT. ");
+            while (true)
+            {
+                var value = Console.ReadLine();
+                if (value == "exit")
+                {
+                    break;
+                }
+
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("The string must not be empty.");
+                }
+                else
+                {
+                    Console.WriteLine(value[0]);
+                }
+            }
         }
     }
 }
