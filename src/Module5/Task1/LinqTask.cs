8fd55db00ff92641e993c0b2832e5f50b641489d
DmitryPanaskov <44893730+DmitryPanaskov@users.noreply.github.com>
2021-05-02 01:16:15 +0300

    Module5

--- src/Module5/Task1/LinqTask.cs
+++ src/Module5/Task1/LinqTask.cs
@@ -6,11 +6,14 @@ namespace Task1
 {
     public static class LinqTask
     {
+        // Выдайте список всех клиентов, чей суммарный оборот (сумма всех заказов) превосходит некоторую величину X.
         public static IEnumerable<Customer> Linq1(IEnumerable<Customer> customers, decimal limit)
         {
             throw new NotImplementedException();
         }
 
+        // Для каждого клиента составьте список поставщиков, находящихся в той же стране и том же городе.
+        // Сделайте задания без использованием группировки.
         public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2(
             IEnumerable<Customer> customers,
             IEnumerable<Supplier> suppliers)
@@ -18,6 +21,8 @@ namespace Task1
             throw new NotImplementedException();
         }
 
+        // Для каждого клиента составьте список поставщиков, находящихся в той же стране и том же городе.
+        // Сделайте задания с использованием группировки.
         public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2UsingGroup(
             IEnumerable<Customer> customers,
             IEnumerable<Supplier> suppliers)
@@ -25,28 +30,38 @@ namespace Task1
             throw new NotImplementedException();
         }
 
+        // Найдите всех клиентов, у которых были заказы, превосходящие по сумме величину X.
         public static IEnumerable<Customer> Linq3(IEnumerable<Customer> customers, decimal limit)
         {
             throw new NotImplementedException();
         }
 
+        // Выдайте список клиентов с указанием, начиная с какой даты они стали клиентами
+        // (принять за таковую дату самого первого заказа)
         public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq4(
             IEnumerable<Customer> customers)
         {
             throw new NotImplementedException();
         }
 
+        //Сделайте предыдущее задание, но выдайте список отсортированным по году,
+        //месяцу, оборотам клиента (от максимального к минимальному) и имени клиента
         public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq5(
             IEnumerable<Customer> customers)
         {
             throw new NotImplementedException();
         }
 
+        // Укажите всех клиентов, у которых указан нецифровой почтовый код или
+        // не заполнен регион или в телефоне не указан код оператора
+        // (для простоты считаем, что это равнозначно «нет круглых скобочек в начале»).
         public static IEnumerable<Customer> Linq6(IEnumerable<Customer> customers)
         {
             throw new NotImplementedException();
         }
 
+        // Сгруппируйте все продукты по категориям, внутри – по наличию на складе,
+        // внутри последней группы отсортируйте по стоимости
         public static IEnumerable<Linq7CategoryGroup> Linq7(IEnumerable<Product> products)
         {
             /* example of Linq7result
@@ -63,6 +78,8 @@ namespace Task1
             throw new NotImplementedException();
         }
 
+        // Сгруппируйте товары по группам «дешевые», «средняя цена», «дорогие».
+        // (0, дешевые] U (дешевые, средняя цена] U (средняя цена, дорогие].
         public static IEnumerable<(decimal category, IEnumerable<Product> products)> Linq8(
             IEnumerable<Product> products,
             decimal cheap,
@@ -72,12 +89,16 @@ namespace Task1
             throw new NotImplementedException();
         }
 
+        // Рассчитайте среднюю прибыльность каждого города (средняя сумма заказов на каждого клиента)
+        // и среднюю интенсивность (среднее количество заказов, приходящееся на клиента из каждого города)
         public static IEnumerable<(string city, int averageIncome, int averageIntensity)> Linq9(
             IEnumerable<Customer> customers)
         {
             throw new NotImplementedException();
         }
 
+        // Соберите строку, состоящую из уникальных названий стран поставщиков,
+        // отсортированную сначала по длине, потом по названию страны.
         public static string Linq10(IEnumerable<Supplier> suppliers)
         {
             throw new NotImplementedException();
