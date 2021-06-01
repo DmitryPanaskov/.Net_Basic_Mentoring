d7164288921783f8fbdba1a10f7ab660b0a4887c
DmitryPanaskov <44893730+DmitryPanaskov@users.noreply.github.com>
2021-06-02 01:05:42 +0300

    Module8
    Add implementation to LCDTranslator.

--- src/Module8/Katas/LCDTranslator.cs
+++ src/Module8/Katas/LCDTranslator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Katas.Interfaces;
 
@@ -7,9 +8,91 @@ namespace Katas
 {
     public class LCDTranslator : ILCDTranslator
     {
+        private readonly StringBuilder line1 = new StringBuilder();
+
+        private readonly StringBuilder line2 = new StringBuilder();
+
+        private readonly StringBuilder line3 = new StringBuilder();
+
         public string Translate(long number)
         {
-            throw new NotImplementedException();
+            if (number > 9999999999 || number < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number), "The value must be between 0 and 9999999999");
+            }
+
+            var arrayLong = number.ToString().Select(digit => long.Parse(digit.ToString()));
+
+            foreach (var digit in arrayLong)
+            {
+                TranslateNumberToLcdString(digit);
+            }
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(line1.ToString());
+            stringBuilder.AppendLine(line2.ToString());
+            stringBuilder.AppendLine(line3.ToString());
+
+            return stringBuilder.ToString();
+        }
+
+        private void TranslateNumberToLcdString(long digit)
+        {
+            switch (digit)
+            {
+                case 0:
+                    line1.Append("._.");
+                    line2.Append("|.|");
+                    line3.Append("|_|");
+                    break;
+                case 1:
+                    line1.Append("...");
+                    line2.Append("..|");
+                    line3.Append("..|");
+                    break;
+                case 2:
+                    line1.Append("._.");
+                    line2.Append("._|");
+                    line3.Append("|_.");
+                    break;
+                case 3:
+                    line1.Append("._.");
+                    line2.Append("._|");
+                    line3.Append("._|");
+                    break;
+                case 4:
+                    line1.Append("...");
+                    line2.Append("|_|");
+                    line3.Append("..|");
+                    break;
+                case 5:
+                    line1.Append("._.");
+                    line2.Append("|_.");
+                    line3.Append("._|");
+                    break;
+                case 6:
+                    line1.Append("._.");
+                    line2.Append("|_.");
+                    line3.Append("|_|");
+                    break;
+                case 7:
+                    line1.Append("._.");
+                    line2.Append("..|");
+                    line3.Append("..|");
+                    break;
+                case 8:
+                    line1.Append("._.");
+                    line2.Append("|_|");
+                    line3.Append("|_|");
+                    break;
+                case 9:
+                    line1.Append("._.");
+                    line2.Append("|_|");
+                    line3.Append("._|");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
